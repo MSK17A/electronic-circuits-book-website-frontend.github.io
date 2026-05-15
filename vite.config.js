7a1874bbcf43acc4b31702b5f0192c867136ab6c
Mohammad AlSammak <msk170a@gmail.com>
2026-05-15 17:57:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import path from "path"
+import path from "path";
 
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import devtools from "solid-devtools/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools(), tanstackRouter({ target: 'solid' }), solid()],
+  base: "electronic-circuits-book-website-frontend",
+  plugins: [devtools(), tanstackRouter({ target: "solid" }), solid()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
